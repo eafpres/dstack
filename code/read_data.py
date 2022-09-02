0c07fca472f0c7bdf58968c64ad27f328a7262c0
blainebateman <eafpres@gmail.com>
2022-09-01 18:12:31 -0600

    Update code to see OS

--- code/read_data.py
+++ code/read_data.py
@@ -9,10 +9,21 @@ Created on Thu Sep  1 17:55:26 2022
 #%% libraries
 #
 import pandas as pd
+import sys
+#
+#%% configure
+#
+my_os = sys.platform
+if my_os == 'linux':
+  path_prefix = '/mnt/c'
+else:
+  path_prefix = 'C:'
+  #
+path = path_prefix + '/eaf llc/aa-analytics and bi/dstack/'
 #
 #%% data
 #
-data = pd.read_csv('data/parabolic_data.csv')
+data = pd.read_csv(path + 'data/parabolic_data.csv')
 #
 #%% stats
 #
