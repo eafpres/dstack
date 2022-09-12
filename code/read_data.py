731f61b01a94b90974b45e1464943ae4297ab032
blainebateman <eafpres@gmail.com>
2022-09-12 15:08:19 -0600

    test save figure

--- code/read_data.py
+++ code/read_data.py
@@ -9,6 +9,16 @@ Created on Thu Sep  1 17:55:26 2022
 #%% libraries
 #
 import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import os
+#
+#%% configure
+#
+my_os = sys.platform
+print('found OS ', my_os)
+print('user is: ', os.environ.get('USER'))
+print('working directory is: ', os.getcwd())
 #
 #%% data
 #
@@ -18,4 +28,10 @@ data = pd.read_csv('data/parabolic_data.csv')
 #
 print('data summary')
 print(data.describe())
+#
+#%% visualize
+#
+fig, ax = plt.subplots(figsize = (9, 9))
+ax.scatter(data['x'], data['y'])
+plt.savefig('parabolic.jpg')
 #
